79131c784d1342fcaf283a2422292aa2d53325fc
Joseph Hanson <jhanson@121g.io>
2026-08-23 15:51:43 -0400

    stashing change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl font-semibold">Grace City Collective</h1>
-      <Button>Get started</Button>
+    <div className="h-dvh p-5 bg-amber-100">
+      <div className="border-8 border-double border-black rounded-2xl w-full h-full ">
+        <div className="flex h-full w-full gap-5 items-baseline justify-baseline p-5">
+          <h1 className="text-3xl font-semibold">DAD LOVES WILDER</h1>
+          <div>
+            <Button>DAD</Button>
+            <Button>DAD</Button>
+            <Button>DAD</Button>
+            <Button>DAD</Button>
+            <Button>DAD</Button>
+            <Button>DAD</Button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
